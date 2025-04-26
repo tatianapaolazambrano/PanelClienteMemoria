9219eaa440224ada6971c6b3f5960339413714a5
tatianapaolazambrano <tatianapaola.zambrano689@comunidadunir.net>
2025-04-26 00:54:18 -0500

    Primer commit con el crud,postman,panel grafico

---
+++ src/main/java/academy/atl/customers/entities/Customer.java
@@ -0,0 +1,20 @@
+package academy.atl.customers.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "customers")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+
+}
